ca4356653e8dafcbc7ee27059027af4d6340862a
Kaue Servico <sobreirak29@gmail.com>
2025-05-26 15:29:23 -0300

    fix: Sintaxe incorretas

--- src/model/script.ts
+++ src/model/script.ts
@@ -1,16 +1,19 @@
 export interface Script {
   name: string;
-  path: string;
+  url?: string;
+  path?: string;
   return_type: string;
 }
 
 export interface ScriptData extends Script {
   id: number;
+  url: string;
+  path: string;
 }
 
 export interface ScriptDialogProps {
   open: boolean;
   onClose: () => void;
-  onSubmit: (script: Script) => void;
+  onSubmit: (script: Script & { id?: number }, file: File | null) => void;
   initialData?: Partial<ScriptData> | null;
 }
